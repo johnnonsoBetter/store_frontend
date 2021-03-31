d2e2eda0ef73370a4e62f5510793224e45babc94
John Nonso <johnnonso090@gmail.com>
2021-03-31 02:50:15 +0000

    expenses table now able to render data from server

--- src/admin/dashboard/contents/home/transaction_activity/expenses/ExpensesTable.js
+++ src/admin/dashboard/contents/home/transaction_activity/expenses/ExpensesTable.js
@@ -1,4 +1,4 @@
-import { Typography } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import TransactionActivityContext from '../../../../../../context/admin/transaction_activity/TransactionActivity'
@@ -8,6 +8,7 @@ import Accordion from '@material-ui/core/Accordion';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {DateTime} from 'luxon'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,18 +16,24 @@ const useStyles = makeStyles((theme) => ({
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
-    flexBasis: '33.33%',
+    flexBasis: '39.33%',
     flexShrink: 0,
+    
   },
   secondaryHeading: {
     fontSize: theme.typography.pxToRem(15),
     color: theme.palette.text.secondary,
+    textTransform: "capitalize"
+   
   },
+  detail: {
+    textTransform: "capitalize"
+  }
 }));
 
 function ExpensesTable(){
     const {storeName} = useParams()
-    const {staticDate, setTransctionActivity} = useContext(TransactionActivityContext)
+    const {staticDate, setTransactionActivity} = useContext(TransactionActivityContext)
    
     const [expenses, setExpenses] = useState([])
     const classes = useStyles();
@@ -51,7 +58,7 @@ function ExpensesTable(){
              console.log(response)
              const {transaction_activity, expenses} = response.data
              console.log(expenses)
-             setTransctionActivity(transaction_activity)
+             setTransactionActivity(transaction_activity)
              setExpenses(expenses)
             
             }).catch(err => {
@@ -71,8 +78,8 @@ function ExpensesTable(){
               console.log(response)
               const {transaction_activity, expenses} = response.data
               console.log(expenses)
-            setTransctionActivity(transaction_activity)
-            setExpenses(expenses)
+              setTransactionActivity(transaction_activity)
+              setExpenses(expenses)
         
               
             }).catch(err => {
@@ -93,7 +100,41 @@ function ExpensesTable(){
     }, [])
 
     return (
-        <Typography style={{color: "white"}}> This is the expenses </Typography>
+
+
+       <Box width="100%">
+
+         {
+           expenses.map(expense => {
+            const {cost, detail, created_at, cashier_name, id} = expense
+            const time =  DateTime.fromISO(created_at).toLocaleString(DateTime.TIME_SIMPLE)
+
+            return (
+               <Box width="100%" key={id} id={id} p={1}>
+                <Accordion expanded={expanded === `panel${id}`} onChange={handleChange(`panel${id}`)}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1bh-content"
+                  id= {`panel${id}-header`}
+                >
+                  <Typography className={classes.heading}>₦{cost}</Typography>
+                  <Box display="flex" width="100%" justifyContent="center">
+                    <Typography className={classes.secondaryHeading}>{cashier_name}</Typography>
+                   </Box>
+                  
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography className={classes.detail}>
+                    {detail}
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              </Box>
+            )
+           })
+         }
+       </Box>
+      
     )
 }
 
