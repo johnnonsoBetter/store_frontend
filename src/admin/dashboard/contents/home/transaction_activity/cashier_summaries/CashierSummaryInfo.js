416c92529fef49dd1ad631ba7d772393606702ad
John <johnnonso090@gmail.com>
2021-04-03 20:15:49 +0100

    created the transaction review info component that renders the transaction review of a sales summary

--- src/admin/dashboard/contents/home/transaction_activity/cashier_summaries/CashierSummaryInfo.js
+++ src/admin/dashboard/contents/home/transaction_activity/cashier_summaries/CashierSummaryInfo.js
@@ -1,13 +1,13 @@
-import { Box, IconButton, Paper, Typography, Avatar, makeStyles, Divider, Badge } from '@material-ui/core'
-import { Clear, Person } from '@material-ui/icons'
+import { Box, IconButton, Paper, Typography, Avatar, makeStyles, Button } from '@material-ui/core'
+import { ArrowBack, Clear, Person } from '@material-ui/icons'
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { activitiesApi } from '../../../../../../api/admin/activities/api'
 import CashierSummaryContext from '../../../../../../context/admin/transaction_activity/cashier_summaries/CashierSummaryContext'
 import Loader from '../../../../Loader'
 import deepOrange from '@material-ui/core/colors/deepOrange'
-import clsx from 'clsx';
 import AmountFormater from '../../../../../../helpers/AmountFormater'
+import TransactionReview from './TransactionReview'
 
 
 
@@ -31,10 +31,10 @@ function CashierSummaryInfo(){
     const [cashierSummary, setCashierSummary] = useState(null)
     const [cashierSystemSummary, setCashierSystemSummary] = useState(null)
     const [transactionReview, setTransactionReview] = useState(null)
+    const [view, setView] = useState('summary')
     const cashierSummaryApi = activitiesApi(storeName, `cashier_sales_summaries/${cashierSummaryId}`)
     const [loading, setLoading] = useState(true)
     const [failed, setFailed] = useState(false)
-    const circle = <div className={clsx()} />
     const classes = useStyles()
 
 
@@ -68,16 +68,25 @@ function CashierSummaryInfo(){
             setTransactionReview(null)
             setLoading(true)
             setFailed(false)
+            setView('summary')
         }
     }, [])
     return (
         <Box width="100%">
-            <Box display="flex">
+            <Box width="100%" display="flex" alignItems="center" justifyContent="space-between">
                 <IconButton onClick={()=> {
                     setDrawerOpened(false)
                 }}>
                     <Clear />
                 </IconButton>
+                
+                {
+                    view !== "summary" ?
+                    <IconButton onClick={()=> setView('summary')} >
+                        <ArrowBack />
+                    </IconButton> : null
+                }
+                
 
             </Box>
 
@@ -90,86 +99,103 @@ function CashierSummaryInfo(){
             
                 </Box> 
             : 
-            
-            <Box textAlign="center"> 
+
+            <>
+
+                {
+                    view === "summary" ? 
+                    <Box textAlign="center"> 
                 
-                <Box display="flex"  justifyContent="center"  marginTop={1} width="100%">
-                
-                    <Box> <Avatar className={classes.large}> {cashierSummary['cashier_name'].toString().toUpperCase().charAt(0)}  </Avatar> </Box>
-                </Box>
-
-                
-
-                
-                <Paper>
-                   <Box p={2} >
-                        <Box marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box> <Person /> </Box>
-                            <Box>  <Typography> {cashierSummary['cashier_name']} </Typography>    </Box>
-                        </Box>
-
-                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Cash At Drawer </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSummary['total_cash_at_hand']).amount() } </Typography>    </Box>
-                        </Box>
-
-                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Pos Submitted </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSummary['total_pos']).amount() } </Typography>    </Box>
-                        </Box>
-
-                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Transfer Submitted </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSummary['total_transfer']).amount() } </Typography>    </Box>
-                        </Box>
-                   </Box>
-                </Paper>
-
-                <Paper>
-                    <Box marginTop={2} p={2}>
-                        <Typography> System </Typography>
-                    <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Total Sales </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_sales']).amount() } </Typography>    </Box>
-                        </Box>
-
-                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Total Expenses </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_expenses']).amount() } </Typography>    </Box>
-                        </Box>
-
-                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Total Debts </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_debts']).amount() } </Typography>    </Box>
-                        </Box>
-
-                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Total Recovered </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_recovered']).amount() } </Typography>    </Box>
-                        </Box>
-
-                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Total Goods Returned </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_goods_returned_cost']).amount() } </Typography>    </Box>
-                        </Box>
-
-                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Total Pos </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_pos']).amount() } </Typography>    </Box>
-                        </Box>
-
-                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Total Transfer </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_transfer']).amount() } </Typography>    </Box>
-                        </Box>
-
-                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
-                            <Box>  <Typography> Total Cash At Hand </Typography>    </Box>
-                            <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_cash_at_hand']).amount() } </Typography>    </Box>
-                        </Box>
+                    <Box display="flex"  justifyContent="center"  marginTop={1} width="100%">
+                    
+                        <Box> <Avatar className={classes.large}> {cashierSummary['cashier_name'].toString().toUpperCase().charAt(0)}  </Avatar> </Box>
                     </Box>
-                </Paper>
-            </Box>
+    
+                    
+    
+                    
+                    <Paper>
+                       <Box p={2} >
+                            <Box marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box> <Person /> </Box>
+                                <Box>  <Typography> {cashierSummary['cashier_name']} </Typography>    </Box>
+                            </Box>
+    
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Cash At Drawer </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSummary['total_cash_at_hand']).amount() } </Typography>    </Box>
+                            </Box>
+    
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Pos Submitted </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSummary['total_pos']).amount() } </Typography>    </Box>
+                            </Box>
+    
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Transfer Submitted </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSummary['total_transfer']).amount() } </Typography>    </Box>
+                            </Box>
+                       </Box>
+                    </Paper>
+    
+                    <Paper>
+                        <Box marginTop={2} p={2}>
+                            <Typography> System </Typography>
+                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Total Sales </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_sales']).amount() } </Typography>    </Box>
+                            </Box>
+    
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Total Expenses </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_expenses']).amount() } </Typography>    </Box>
+                            </Box>
+    
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Total Debts </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_debts']).amount() } </Typography>    </Box>
+                            </Box>
+    
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Total Recovered </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_recovered']).amount() } </Typography>    </Box>
+                            </Box>
+    
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Total Goods Returned </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_goods_returned_cost']).amount() } </Typography>    </Box>
+                            </Box>
+    
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Total Pos </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_pos']).amount() } </Typography>    </Box>
+                            </Box>
+    
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Total Transfer </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_transfer']).amount() } </Typography>    </Box>
+                            </Box>
+    
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Total Cash At Hand </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(cashierSystemSummary['total_cash_at_hand']).amount() } </Typography>    </Box>
+                            </Box>
+                        </Box>
+                    </Paper>
+    
+                    <Box display="flex" p={1} marginTop={2} width="100%">
+                        <Button onClick ={()=> setView("review")} style={{width: "100%", color: "white", backgroundColor: "#654ab9"}} > View Transaction BreakDown</Button>
+                    </Box>
+                </Box>
+                    : 
+                    
+                    <TransactionReview setView={setView} transactionReviewInfo={transactionReview} />
+                }
+
+
+            </>
+            
+            
             }
             
         </Box>
---
+++ src/admin/dashboard/contents/home/transaction_activity/cashier_summaries/TransactionReview.js
@@ -0,0 +1,161 @@
+import { Box, Paper, Slide, Typography, Badge} from '@material-ui/core'
+import React from 'react'
+import clsx from 'clsx';
+import deepOrange from '@material-ui/core/colors/deepOrange'
+import AmountFormater from '../../../../../../helpers/AmountFormater'
+import { Check, Remove } from '@material-ui/icons';
+
+function TransactionReview(props){
+    const {setView, transactionReviewInfo} = props
+    const circle = <div className={clsx()} />;
+
+    
+    return (   
+        <Slide in={true} direction="left">
+            <Box width="100%" >
+                <Box textAlign="center"> 
+                    <Typography variant="h6"> Review </Typography>
+                </Box>
+
+                <Paper >
+
+                    <Box p={2}>
+                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                            <Box>  <Typography> System Total </Typography>    </Box>
+                            <Box>  <Typography> ₦{ AmountFormater(transactionReviewInfo['system_total_amount']).amount() } </Typography>    </Box>
+                        </Box>
+
+                        <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                            <Box>  <Typography> Cashier Total </Typography>    </Box>
+                            <Box>  <Typography> ₦{ AmountFormater(transactionReviewInfo['cashier_total_amount']).amount() } </Typography>    </Box>
+                        </Box>
+                        
+
+                    </Box>
+                    
+
+                </Paper>
+                
+                <Box marginTop={2}>
+                    <Paper >
+                        
+                        
+                        <Box p={2}>
+                            <Box textAlign="center" variant="h6"> <Typography> Transaction Differences  </Typography></Box>
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Pos </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(transactionReviewInfo['pos_differences']).amount() } </Typography>    </Box>
+                            </Box>
+
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Transfer </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(transactionReviewInfo['transfer_differences']).amount() } </Typography>    </Box>
+                            </Box>
+
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Cash </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(transactionReviewInfo['cash_at_hand_differences']).amount() } </Typography>    </Box>
+                            </Box>
+
+                            
+
+                        </Box>
+                        
+
+                    </Paper>
+
+
+                </Box>
+
+
+                 
+                <Box marginTop={2}>
+                    <Paper >
+                        
+                        
+                        <Box p={2}>
+                            <Box textAlign="center" variant="h6"> <Typography> Final Outcome  </Typography></Box>
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Differences </Typography>    </Box>
+                                <Box>  <Typography> ₦{ AmountFormater(transactionReviewInfo['total_amount_difference']).amount() } </Typography>    </Box>
+                            </Box>
+
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Outcome </Typography>    </Box>
+                                <Box>
+                                       <Typography style={{textTransform: "capitalize"}}> {transactionReviewInfo['final_outcome']} </Typography>    
+                                </Box>
+                            </Box>
+
+                            
+
+                        </Box>
+                        
+
+                    </Paper>
+
+
+                </Box>
+
+
+                <Box marginTop={2}>
+                    <Paper >
+                        
+                        
+                        <Box p={2}>
+                            <Box textAlign="center" variant="h6"> <Typography> Reconcilation  </Typography></Box>
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Reconciled ? </Typography>    </Box>
+                                <Box>  
+                                    <Typography> 
+                                    
+                                    {transactionReviewInfo['reconciled'] ?
+                                        <Check  /> : <Remove  />
+                                    } 
+                                    
+                                    </Typography>    
+                                </Box>
+                                
+                            </Box>
+
+                            <Box  marginTop={2} display="flex"  justifyContent="space-between">
+                                <Box>  <Typography> Reconciled Outcome </Typography>    </Box>
+                                <Box>
+                                    
+                                    {transactionReviewInfo['reconciled_outcome'] === null && <Typography> {transactionReviewInfo['reconciled_outcome'] }  </Typography> }
+                                   
+                                </Box>
+                            </Box>
+
+                            
+
+                                
+                            
+
+                        </Box>
+                        
+
+                    </Paper>
+
+
+                </Box>
+
+                <Box p={2}  marginTop={2} display="flex"  justifyContent="space-between">
+                    <Box>  <Typography> Money Remitted </Typography>    </Box>
+                    <Box>
+                    <Typography> ₦{ AmountFormater(transactionReviewInfo['money_remitted']).amount() } </Typography>
+                    </Box>
+                </Box>
+                
+                
+
+                
+                               
+               
+            </Box>
+        </Slide>
+        
+    )
+}
+
+export default TransactionReview
