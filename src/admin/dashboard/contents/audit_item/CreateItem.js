d6ec9eed32de5faeeb886099af0ed82f5266110d
John <johnnonso090@gmail.com>
2021-05-18 10:27:47 +0100

    made changes to the input box of the admin create item

--- src/admin/dashboard/contents/audit_item/CreateItem.js
+++ src/admin/dashboard/contents/audit_item/CreateItem.js
@@ -1,5 +1,5 @@
 import {React, useState, useContext} from 'react'
-import {Box, Drawer, InputAdornment, InputLabel, OutlinedInput, TextField, Button, MenuItem, Typography, IconButton, useMediaQuery, makeStyles} from '@material-ui/core'
+import {Box, Drawer, InputAdornment, InputLabel, OutlinedInput, TextField, Button, MenuItem, Typography, IconButton, useMediaQuery, makeStyles, withStyles, InputBase} from '@material-ui/core'
 import CloseIcon from '@material-ui/icons/Close'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 import AuditModeContext from '../../../../context/audit_item/AuditModeContext'
@@ -7,6 +7,40 @@ import { itemApi } from '../../../../api/admin/item/api'
 import { green } from '@material-ui/core/colors';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+
+
+
+ const Input = withStyles((theme) => ({
+    root: {
+      'label + &': {
+        marginTop: theme.spacing(3),
+      },
+    },
+    input: {
+      borderRadius: 4,
+      position: 'relative',
+      backgroundColor: '',
+      border: '1px solid #ced4da',
+      borderColor: '#a0a0a0a1',
+      color: "black",
+      height: "42px",
+      fontSize: 16,
+      padding: '7px 10px 5px 7px',
+      borderRadius: 5,
+      textTransform: "capitalize",
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+        'Kanit',
+        'cursive',
+      ].join(','),
+      '&:focus': {
+        borderRadius: 5,
+        
+        
+      },
+    },
+  }))(InputBase);
 
 
   
@@ -36,7 +70,7 @@ const useStyles = makeStyles((theme) => ({
         left: '50%',
         marginTop: -12,
         marginLeft: -12,
-      },
+      }
 }))
 
 function CreateItem(){
@@ -236,11 +270,12 @@ function CreateItem(){
                      
                         <Box display="flex"  m={1} justifyContent="center">
                             <Box m={1} > 
-                                <TextField  id="outlined-basic" name="name" label="Name" variant="outlined" onChange={handleChange} value={item['real_item'].name}/>
+                                <Input name="name"placeholder="Name" variant="outlined" onChange={handleChange} value={item['real_item'].name} />
                             </Box>
 
                             <Box m={1}>
-                                <TextField id="outlined-basic" name="barcode"  label="Barcode" variant="outlined" onChange={handleChange} value={item['real_item'].barcode}/>
+                                <Input id="outlined-basic" name="barcode" placeholder="Barcode"  label="Barcode" variant="outlined" onChange={handleChange} value={item['real_item'].barcode} />
+
                             </Box>
                         </Box>
 
