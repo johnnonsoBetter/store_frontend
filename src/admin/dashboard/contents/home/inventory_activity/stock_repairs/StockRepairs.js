c04392afd562133c6d79bc893486f0d903e0d118
John <johnnonso090@gmail.com>
2021-05-20 13:29:36 +0100

    created the componentt for the stock repair item

--- src/admin/dashboard/contents/home/inventory_activity/stock_repairs/StockRepairs.js
+++ src/admin/dashboard/contents/home/inventory_activity/stock_repairs/StockRepairs.js
@@ -1,30 +1,97 @@
-import { Box, CircularProgress, Grid, makeStyles, Typography } from '@material-ui/core'
+import { Avatar, Box, Button, CircularProgress, Grid, InputBase, makeStyles, Menu, MenuItem, Typography, withStyles } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { activitiesApi } from '../../../../../../api/admin/activities/api'
 
 
+
+export const Input = withStyles((theme) => ({
+    root: {
+      'label + &': {
+        marginTop: theme.spacing(3),
+      },
+    },
+    input: {
+      borderRadius: 4,
+      position: 'relative',
+      backgroundColor: '',
+      border: '1px solid #ced4da',
+      borderColor: '#a0a0a0a1',
+      color: "#e0bb30",
+      fontSize: 16,
+      padding: '7px 10px 5px 7px',
+      borderRadius: 5,
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+       //Use the system font instead of the default Roboto font.
+      fontFamily: [
+        'Kanit',
+        'cursive',
+      ].join(','),
+      '&:focus': {
+        borderRadius: 5,
+        
+        
+      },
+    },
+  }))(InputBase);
+
+
 const useStyles = makeStyles((theme) => ({
     stockRepair: {
-        backgroundColor: "#101d68",
+        backgroundColor: "#373d3f",
         borderRight: "1px solid",
         borderRadius: 5
     },
     stockRepairCont: {
         height: "calc(74vh - 200px)",
         overflowY: "auto"
-    }
+    },
+    blackCircle: {
+        width: theme.spacing(4.5),
+        height: theme.spacing(4.5),
+        backgroundColor: "black"
+
+    },
+    
+    redCircle: {
+        width: theme.spacing(4.5),
+        height: theme.spacing(4.5),
+        backgroundColor: "red"
+
+    },
+    
+    greenCircle: {
+        width: theme.spacing(4.5),
+        height: theme.spacing(4.5),
+        backgroundColor: "green"
+
+    },
+    
+    orangeCircle: {
+        width: theme.spacing(4.5),
+        height: theme.spacing(4.5),
+        backgroundColor: "orange"
+
+    },
 }))
+
+
+
 
 function StockRepairs(){
 
     const [loading, setLoading] = useState(false)
     const [failed, setFailed] = useState(false)
     const [stockRepairs, setStockRepairs] = useState([])
+    const [anchorEl, setAnchorEl] = useState(null);
 
     const {storeName} = useParams()
     const activity = activitiesApi(storeName, 'item_stock_repairs')
     const classes = useStyles()
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
 
     useEffect(()=> {
@@ -32,6 +99,9 @@ function StockRepairs(){
         activity.load().then((response => {
 
             console.log(response)
+            const {item_stock_repairs} = response.data
+
+            setStockRepairs(item_stock_repairs)
         })).catch(err => {
 
             console.log(err)
@@ -40,9 +110,41 @@ function StockRepairs(){
 
     }, [])
 
-    return (
+
+
+    function classesType(classes, outcome){
+        console.log(outcome)
+        const value = outcome.toLowerCase()
+        return value === "shortage" ? classes.redCircle : value === "excess" ? classes.orangeCircle : value === "balanced" ? classes.greenCircle : null
+    }
+
+  return (
         <Box>
             <Box p={2} textAlign="left"> <Typography variant="h5"> Stock Repairs </Typography></Box>
+            <Box>
+                <Input placeholder="Search Item"/>
+                <Box display="flex"  width="100%" >
+                  
+                  <Button style={{color: "white"}} aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                    BU
+                  </Button>
+                  <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={()=>  setAnchorEl(null)}
+                  >
+                   
+                    <MenuItem >Shortage</MenuItem>
+                    <MenuItem >Excess</MenuItem>
+                    <MenuItem >Balanced</MenuItem>
+                    
+                  </Menu>
+             
+                  
+                </Box>
+            </Box>
 
 
             {
@@ -51,21 +153,42 @@ function StockRepairs(){
                 <Box display="flex" alignItems="center" justifyItems="center"> <Typography> Failed To Load Stock Repairs </Typography> </Box>  :
                 <Box marginTop={3} className={classes.stockRepairCont}>
            
-                <Grid spacing={1} container >
+                <Grid spacing={2} container >
                     {
-                        // stockRepairs.map(restock => {
+                         stockRepairs.map(restock => {
 
-                        //     const {id, item_name, quantity, created_at} = restock
+                             const {id, name, before_repair_quantity, repaired_quantity, repair_quantity_outcome, repaired_outcome, created_at} = restock
 
-                        //     return (
-                        //         <Grid  item xs={12} md={6} key={id} >
-                        //             <Box className={classes.restock}  p={2} display="flex" justifyContent="space-around">
-                        //                 <Typography> {item_name} </Typography>
-                        //                 <Typography> {quantity} </Typography>
-                        //             </Box>
-                        //         </Grid>
-                        //     )
-                        // })
+                             return (
+                                 <Grid  item xs={12} md={6} key={id} >
+                                     <Box className={classes.stockRepair}  p={1} >
+                                            <Box p={1}>
+                                                <Typography> {name} </Typography>
+                                            </Box>
+
+                                        <Box display="flex" alignItems="center" justifyContent="space-around">
+                                            <Box>
+                                                <Avatar className={classes.blackCircle}>{before_repair_quantity}</Avatar>
+                                            </Box>
+                                            
+                                            <Box>
+                                                <Avatar className={classes.blackCircle}>{repaired_quantity}</Avatar>
+                                            </Box>
+                                        
+                                        </Box>    
+                                        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                                            
+                                            <Box p={1}>
+                                                <Avatar className={classesType(classes, repaired_outcome)}>{repair_quantity_outcome}</Avatar>
+                                            </Box>
+                                            
+                                            <Typography style={{textTransform: "capitalize"}}>{repaired_outcome}</Typography>
+                                        </Box>
+
+                                     </Box>
+                                 </Grid>
+                             )
+                         })
                     }
                 </Grid>
                 
